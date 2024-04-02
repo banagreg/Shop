c1548d64b0087389a4f6470358d002a5e08a0b50
banagreg <bogdanovagergana97@gmail.com>
2024-04-02 10:56:32 +0300

    Added ProductCard component

--- clothing-shop/src/App.js
+++ clothing-shop/src/App.js
@@ -2,6 +2,7 @@ import Navigation from './routes/Navigation/Navigation';
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/Home/Home';
 import Authentication from './routes/Authentication/Authentication';
+import Shop from './routes/Shop/Shop';
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
 
